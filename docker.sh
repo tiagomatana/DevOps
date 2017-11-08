2b62d3a9483cc914833e2ff8928f320cf33e28b3
Tiago Matana <tiago.matana@gmail.com>
2017-11-08 18:52:14 -0200

    docker

--- docker.sh
+++ docker.sh
@@ -5,25 +5,7 @@ while true; do
 echo "==========================================================
         Digite o número do container a ser instalado:
         1 - Linda
-        2 - Atom
-        3 - Eclipse
-        4 - NodeJS
-        5 - NPM
-        6 - Postman
-        7 - Robo3T
-        8 - Visual Studio Code
-        9 - Meld
-        10 - Intellij IDEA Community
-        11 - Intellij IDEA Ultimate
-        12 - Vim
-        13 - Git
-        14 - Maven
-        15 - Tmux
 
-        EXTRAS
-        21 - Spotify
-        22 - HipChat
-        0 - Sair
 ==========================================================="
 echo -n "->OPÇÃO:  "
 
@@ -38,8 +20,6 @@ fi
 case $opcao in
     1)
         sudo docker run --name linda -i -t --net=host -p 5432:5432 -p 8080:8080 otussolutions/linda:1.0 /bin/bash;;
-    2)
-        sudo add-apt-repository ppa:webupd8team/atom -y && sudo apt-get update && sudo apt-get install atom -y;;
   
     0)
         echo "Saindo..."
--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ case $opcao in
         sudo rm -rf java-7-oracle.tar.xz
         sudo mv java-7-oracle /usr/lib/jvm/;;
     17)
-        sudo apt-get install docker
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
         sudo apt-get update
         sudo apt-get install docker-ce;;
     21)
