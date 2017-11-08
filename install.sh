33bd9fb917e69bc5caa78117bb2dfe3511900e53
Tiago Matana <tiago.matana@gmail.com>
2017-11-08 14:08:16 -0200

    Hipchat alterado

--- install.sh
+++ install.sh
@@ -105,10 +105,9 @@ case $opcao in
         sudo apt-get update
         sudo apt-get install spotify-client;;
     22)
-        sudo sh -c echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list
-        wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
-        sudo apt-get update
-        sudo apt-get install hipchat4;;
+        wget https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client/pool/HipChat4-4.0.1517-Linux.deb
+	sudo dpkg -i HipChat4-4.0.1517-Linux.deb
+	sudo rm -rf HipChat4-4.0.1517-Linux.deb;;
     0)
         echo "Saindo..."
         exit;;
