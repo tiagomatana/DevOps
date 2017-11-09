5204d89d09d194c458bb446065a672d6caec0cb4
Tiago Matana <tiago.matana@gmail.com>
2017-11-09 10:23:45 -0200

    Add docker-ce for Ubuntu 17.10

--- install.sh
+++ install.sh
@@ -110,11 +110,15 @@ case $opcao in
         sudo apt-get install tmux;;
     16)
         wget http://tiagomatana.esy.es/java7/java-7-oracle.tar.xz
-        tar xvzf java-7-oracle.tar.xz
+        tar -xvzf java-7-oracle.tar.xz
         sudo rm -rf java-7-oracle.tar.xz
         sudo mv java-7-oracle /usr/lib/jvm/;;
     17)
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update
+        sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo apt-key fingerprint 0EBFCD88
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
         sudo apt-get update
         sudo apt-get install docker-ce;;
     21)
